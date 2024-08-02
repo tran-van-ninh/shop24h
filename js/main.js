eae063c9090f2240c42473a36f9793911d152997
tran-van-ninh <dinhmanhninhtt@gmail.com>
2024-08-03 02:42:20 +0700

    shop24h

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.$ = document.querySelector.bind(document);
+window.$$ = document.querySelectorAll.bind(document);
+
+// let navBar = $$("#navbar li a");
+// navBar.forEach(element => {
+//     element.addEventListener("mouseover", element => {
+//         element.target.classList.add("active");
+//     });
+
+//     element.addEventListener("mouseout", element => {
+//         element.target.classList.remove("active");
+//     });
+// });
